8fcf22dacf5005e284dd0a844ad7afd72ef3482c
Xiaodanguo2020 <guo.xiaodan.bk@gmail.com>
2023-01-25 14:37:46 +0800

    challenge broken record

--- async.js
+++ async.js
@@ -36,9 +36,12 @@ function helloGoodbye() {
 
 function brokenRecord() {
   // ADD CODE HERE
+	return setInterval(() => {
+		console.log('hi again')
+	}, 1000)
 }
 // Uncomment the following line to check your work!
-// brokenRecord(); // should log (every second): hi again
+brokenRecord(); // should log (every second): hi again
 
 
 /* CHALLENGE 5 */
